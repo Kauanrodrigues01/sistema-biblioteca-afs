a47e5fcd79d479fd9e9401418d9917efe2e044a7
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-03-24 23:01:11 -0300

    dockerized the application, created Dockerfile and configured Docker Compose

--- core/management/commands/populate_books.py
+++ core/management/commands/populate_books.py
@@ -18,8 +18,15 @@ class Command(BaseCommand):
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
 
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        try:
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=chrome_options)
+        except Exception as e:
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--remote-debugging-port=9222")
+            chrome_options.add_argument("--disable-software-rasterizer")
+            service = Service('/usr/bin/chromedriver')
+            driver = webdriver.Chrome(options=chrome_options, service=service)
 
         url = "https://rafalito92.github.io/bibliotecaafs/"
         driver.get(url)
