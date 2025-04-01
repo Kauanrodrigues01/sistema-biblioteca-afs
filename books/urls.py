081ef089c06f1c89877617029a4d02bd429614ec
gledyson007 <fgledyson5@gmail.com>
2025-04-01 19:49:34 -0300

    Criando a home page

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import login_view, create_loan, list_loan, logout, delete_loan
+from .views import login_view, create_loan, list_loan, logout, delete_loan, home
 
 
 
 urlpatterns = [
+     path('home/', home, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', logout, name='logout'),
     path('emprestimos/criar/', create_loan, name='create_loan'),
