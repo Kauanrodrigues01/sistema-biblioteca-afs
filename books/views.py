4b7ef1d7af5af88ba5cd4731d54674054d3d9bfc
Kauan Rodrigues Lima <162514268+Kauanrodrigues01@users.noreply.github.com>
2025-05-05 12:50:09 -0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -81,7 +81,7 @@ def delete_loan(request, emprestimo_id):
 
     # Libera o livro antes de deletar
     book = emprestimo.book
-    book.disponivel = True
+    book.available = True
     book.save()
 
     # Deleta o empréstimo
